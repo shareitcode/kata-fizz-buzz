2621fc5ccfb0dff27c8278d0aadeadd26b4db9cb
Jean-Baptiste MATHIEU <jb.mathieu@sully-group.fr>
2020-07-23 14:54:35 +0200

    [FizzBuzz] Start testing : number divisbile by three

--- Kata.FizzBuzz.Specs/FizzBuzz.feature
+++ Kata.FizzBuzz.Specs/FizzBuzz.feature
@@ -1,1 +1,6 @@
-﻿
+﻿Feature: FizzBuzz transformed integer to string
+
+	Scenario: Number divisible by three should be Fizz
+		Given a number 15
+		When is divisible by three
+		Then the result should be Fizz
---
+++ Kata.FizzBuzz.Specs/FizzBuzzTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace Kata.FizzBuzz.Specs
+{
+    [Binding]
+    public class FizzBuzzTest
+    {
+        private int number;
+        private string result;
+
+        [Given(@"a number (.*)")]
+        public void GivenANumber(int number)
+        {
+            this.number = number;
+        }
+
+        [When(@"is divisible by three")]
+        public void WhenIsDivisibleByThree()
+        {
+            this.result = number % 3 == 0 ? "Fizz" : number.ToString();
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(string expect)
+        {
+            Assert.AreEqual(expect, this.result);
+        }
+    }
+}
