0627d9b13bf169e764bdb9b31de0db6fd71fe9ab
shareitcode <45316299+shareitcode@users.noreply.github.com>
2021-05-02 15:52:15 +0200

    [GameOfLife] Add GameOfLife kata

---
+++ GameOfLife/Katas.GameOfLife.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Katas.GameOfLife.ConsoleApp
+{
+	// Kata from : https://codingdojo.org/kata/GameOfLife/
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ GameOfLife/Katas.GameOfLife.Tests/GameOfLifeTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katas.GameOfLife.Tests
+{
+	[TestClass]
+	public class GameOfLifeTests
+	{
+		[TestMethod]
+		public void TestMethod()
+		{
+		}
+	}
+}
